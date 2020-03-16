42b0ba64b99f2b0d1e8bacd8587d76ea8f98da97
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-16 14:11:14 +0200

    model

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -1,40 +1,51 @@
-const sequelize = require('sequelize');
+require('dotenv').config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(`${process.env.DB_NAME}`, "postgres", " ", {
+    dialect: "postgres",
+    host: "127.0.0.1"
+});
 
 const Ticket = sequelize.define("ticket", {
     id: {
-        type: sequelize.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
         allowNull: false
     },
     origin: {
-        type: sequelize.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
         len: [2,3]
     },
     destination: {
-        type: sequelize.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
         len: [2,3]
     },
     depart_date: {
-        type: sequelize.DATE,
+        type: Sequelize.DATE,
         allowNull: false,
     },
     return_date: {
-        type: sequelize.DATE,
+        type: Sequelize.DATE,
         allowNull: false
     },
     price: {
-        type: sequelize.DOUBLE,
+        type: Sequelize.DOUBLE,
         allowNull: false
     },
     airline: {
-        type: sequelize.STRING,
+        type: Sequelize.STRING,
         allowNull: false
     },
     flight_number: {
-        type: sequelize.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: false
     }
 });
+    sequelize.sync().then(result => {
+        console.log("Connected to db and sync`d");
+    })
+    .catch(err => {
+        console.log("Something failed during DB connection" + err);
+    });
