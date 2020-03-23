75c756942e139c8bd9bb25f515e401f72ce39459
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-23 11:15:00 +0200

    refactor: unused variables

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_DIALECT} = process.env;
-let db = {}
+
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: 'localhost',
