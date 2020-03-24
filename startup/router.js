b968250c909dc462a26f3aced764f88b3fbf3a1c
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-24 11:03:53 +0200

    master: deploy alpha

--- startup/router.js
+++ startup/router.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const root = require('../routes/root');
-const users = require('../routes/users');
+//const users = require('../routes/users');
 const middlewares = require('../middlewares/routesMiddleware');
-const passport = require('passport');
-require('../middlewares/passport')(passport);
 module.exports = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(middlewares.envCheck);
-    app.use(passport.initialize());
     app.use('/', root);
-    app.use('/users', users);
+ // app.use('/users', users);
     app.use(middlewares.notFound);
     app.use(middlewares.errorMiddleware);
 };
