c4735d3b0f3f78527a9ef14c9a8ba8af4a60630e
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-24 11:21:12 +0200

    master: port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('./startup/logging')();
 require('./startup/router')(app);
 
 
-app.listen(process.env.PORT || 1337, () => {
+app.listen(process.env.PORT || 1488, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
 
