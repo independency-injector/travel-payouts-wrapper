0f47a1ab08676184340b7ae08c0ab51181dfbb1c
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-25 10:01:06 +0200

    master: added CORS support

--- startup/router.js
+++ startup/router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const root = require('../routes/root');
 //const users = require('../routes/users');
 const middlewares = require('../middlewares/routesMiddleware');
+const cors = require('cors');
 module.exports = (app) => {
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(middlewares.envCheck);
