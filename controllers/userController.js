6987d8a7a06b2e6267d6fdffd199bd9cddd3cde1
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-03-25 15:01:38 +0200

    feature: controller methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,12 +22,27 @@ const getUser = async (req, res) => {
     let err, user;
     [err, user] = await to(User.findOne({ where: { username: body.username} }));
     if(err) { return error(res, err.message) };
+    console.log(user);
     return success(res, { user: user }, 201);
 };
 
 const updatePassword = async (req, res) => {
     const { body } = req;
-    if(!body.username || !body.oldPassword) { return error(res, 'Provide username and password', 400)};
+    if(!body.email || !body.oldPassword) { return error(res, 'Provide email and password', 400)};
+    let err, user;
+    [err, user] = await to(User.findOne({ where: { email: body.email } }));
+    if(err) { return error(res, 'No such user found.', 404)};
+    [err, user] = await to(User.validatePassword(user.password));
+    if(err) return error(res, err.message);
+    console.log
+}
+
+const deleteUser = async (req, res) => {
+    const { body } = req;
+    if(!body.email) { return error(res, 'Provide email of the user you want to delete', 400); };
+    let err, user;
+    [err, user] = await to(User.findOne({ where: { email: body.email} } ));
+    if(err) { return error(res, err.message) };
 }
 
 module.exports = {
