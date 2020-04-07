b79492f400ba7415845352401eea071dc4429934
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-07 12:50:10 +0300

    feature: ticket model schema

--- models/ticket.js
+++ models/ticket.js
@@ -3,10 +3,14 @@ const Sequelize = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
     let Model = sequelize.define('ticket', {
-        //schema
+        origin_city: DataTypes.STRING,
+        destination_city: DataTypes.STRING,
+        depart_date: DataTypes.DATE,
+        return_date: DataTypes.DATE,
+        price: DataTypes.FLOAT,
+        airline_id: DataTypes.INTEGER,
+        flight_number: DataTypes.INTEGER
     });
-
-    
 
     return Model;
 }
