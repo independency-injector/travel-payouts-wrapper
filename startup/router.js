f57e8f3da01d24d26bd1c15f5ae8760232656b30
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-08 16:18:45 +0300

    feature/tickets: fixed duplicates check

--- startup/router.js
+++ startup/router.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const root = require('../routes/root');
 const users = require('../routes/users');
-const routeMiddlewares = require('../middlewares/routesMiddleware');
+const middlewares = require('../middlewares/routesMiddleware');
 const cors = require('cors');
 const passport = require('passport');
 module.exports = (app) => {
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-    app.use(routeMiddlewares.envCheck);
+    app.use(middlewares.envCheck);
     app.use(passport.initialize());
     app.use('/', root);
     app.use('/users', users);
-    app.use(routeMiddlewares.notFound);
-    app.use(routeMiddlewares.errorMiddleware);
+    app.use(middlewares.notFound);
+    app.use(middlewares.errorMiddleware);
 };
