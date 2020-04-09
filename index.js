e154104dc4e3601ad7066cc9dbda2c18d539c7c7
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-09 19:29:57 +0300

    master: fixed db synchronization

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ db.sequelize.authenticate().then(() => {
     console.log('Connected to db');
 }).catch(err => console.log('Error occured while connecting to db' + err.message));
 
-db.Ticket.sync().then(() => console.log('Successfully sync`d with db')).catch(err => console.log('Couldn`t sync with db:' + err.message));
+db.sequelize.sync().then(() => console.log('Successfully sync`d with db')).catch(err => console.log('Couldn`t sync with db:' + err.message));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${process.env.PORT || 3000}`);
